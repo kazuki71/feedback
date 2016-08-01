21c0e4db12def3ddad5f1e7f7cfaf4af783fa00f
kazuki71 <kaneokaka@gmail.com>
2016-08-01 10:25:08 -0700

    add handle_failure function

--- feedbacktester.py
+++ feedbacktester.py
@@ -18,6 +18,8 @@ def parse_args():
                             help = 'Random seed (default = None).')
 	parser.add_argument('-t', '--timeout', type = int, default = 3600,
                             help = 'Timeout in seconds (300 default).')
+        parser.add_argument('-I', '--internal', type = bool, default = False,
+                            help = 'Produce internal coverage report at the end (default = False).')
 	parsed_args = parser.parse_args(sys.argv[1:])
 	return (parsed_args, parser)
 
@@ -37,7 +39,7 @@ def covered(pool):
 			pool[4][b] = 1
 
 def createNewPool():
-	return [SUT.sut(), 0.0, [[]], [], dict(), 0.0]
+	return [SUT.sut(), [[]], [], 0.0, dict(), 0.0]
 
 def deletePools(pools, num):
 	newpools = []
@@ -49,7 +51,7 @@ def deletePools(pools, num):
 	for pool in pools:
 		if len(pool[4]) != 0:
 			pool[5] = getUniquness(pool, pools)
-	sortedpools = sorted(pools, key = lambda x : x[5])
+	sortedpools = sorted(pools, key = lambda x : x[5], reverse = True)
 	while len(newpools) < num:
 		for pool in sortedpools:
 			if len(pool[4]) != 0:
@@ -57,10 +59,10 @@ def deletePools(pools, num):
 	return newpools
 
 def feedback(pool, keys):
-	global config, R, start
+	global config, fid, R, start
 	elapsed = time.time()
 	sut = pool[0]
-	seq = R.choice(pool[2])[:]
+	seq = R.choice(pool[1])[:]
 	sut.replay(seq)
 	if time.time() - start > config.timeout:
 		return
@@ -68,29 +70,32 @@ def feedback(pool, keys):
 		n = R.randint(2, 100)
 	else:
 		n = 1
+	skipped = 0
 	for i in xrange(n):
 		a = sut.randomEnabled(R)
 		key = getKey(seq, a, sut)
 		if key in keys:
+			skipped += 1
 			continue
 		seq.append(a)
 		if not sut.safely(a):
-			print "FIND LOG BUG in ", time.time() - start, "seconds"
 			keys.add(key)
-			pool[3].append(seq)
-			pool[1] += (time.time() - elapsed)
-			return
-		if time.time() - start > config.timeout:
-			return
-	if n == 1 and key in keys:
+			pool[2].append(seq)
+			pool[3] += (time.time() - elapsed)
+			fid = handle_failure(sut, fid)
+			if time.time() - start > config.timeout:
+				return
+	if n == skipped:
 		return
 	keys.add(key)
-	covered(pool)
-	pool[2].append(seq)
-	pool[1] += (time.time() - elapsed)
+	if config.inp != 1:
+		covered(pool)
+	pool[1].append(seq)
+	pool[3] += (time.time() - elapsed)
+	return
 
 def getKey(seq, a, sut):
-	key = 0
+	key = long()
 	for i in seq:
 		key = getKeyHelper(key, sut.actOrder(i))
 	return getKeyHelper(key, sut.actOrder(a))
@@ -104,9 +109,35 @@ def getKeyHelper(key, aorder):
 	return key
 
 def getScore(pool):
-	if pool[1] == 0.0 or len(pool[0].allBranches()) == 0:
+	if pool[3] == 0.0 or len(pool[0].allBranches()) == 0:
 		return float('inf')
-	return len(pool[0].allBranches()) / pool[1]
+	return len(pool[0].allBranches()) / pool[3]
+
+def getUniquness(pool, pools):
+	uniquness = 0.0
+	for c in pool[4]:
+		uniquness += getUniqunessHelper(pool, c, pools)
+	return uniquness / len(pool[4])
+
+def getUniqunessHelper(pool, c, pools):
+	totalcovered = 0.0
+	for p in pools:
+		if c in p[4]:
+			totalcovered += p[4][c]
+	return pool[4][c] / totalcovered
+
+def handle_failure(sut, fid):
+	filename = 'failure' + `fid` + '.test'
+	sut.saveTest(sut.test(), filename)
+	return fid + 1
+
+def internal(pools):
+	i = 0
+	for pool in pools:
+		print "Producing internal coverage report for pool", i, "..."
+		pool[0].internalReport()
+		print ""
+		i += 1
 
 def selectPool(pools):
 	maxscore = -1.0
@@ -119,25 +150,13 @@ def selectPool(pools):
 			selected = pool
 	return selected
 
-def getUniquness(pool, pools):
-	uniquness = 0.0
-	for c in pool[4]:
-		uniquness += getUniqunessHelper(pool, c)
-	return uniquness / len(pool[4])
-
-def getUniqunessHelper(pool, c, pools):
-	totalcovered = 0
-	for p in pools:
-		if c in p[4]:
-			totalcovered += p[4][c]
-	return pool[4][c] / totalcovered
-
 def main():
-	global config, R, start
+	global config, fid, R, start
 	parsed_args, parser = parse_args()
 	config = make_config(parsed_args, parser)
 	print('Feedback-controlled random testing using config={}'.format(config))
 	R = random.Random(config.seed)
+	fid = 0
 	start = time.time()
 	pools = []
 	keys = set()
@@ -145,16 +164,22 @@ def main():
 		pools.append(createNewPool())
 	lastadded = time.time()
 	while time.time() - start < config.timeout:
-		if time.time() - lastadded > config.nseconds:
+		if config.inp != 1 and time.time() - lastadded > config.nseconds:
 			pools.append(createNewPool())
 			lastadded = time.time()
-		feedback(selectPool(pools), keys)
+		if config.inp != 1:
+			pool = selectPool(pools)
+		else:
+			pool = pools[0]
+		feedback(pool, keys)
 		if time.time() - start > config.timeout:
 			break
-		if len(pools) > config.mnp:
+		if config.inp != 1 and len(pools) > config.mnp:
 			pools = deletePools(pools, config.mnp / 2)
 		if time.time() - start > config.timeout:
 			break
+	if config.internal:
+		internal(pools)
 
 if __name__ == '__main__':
 	main()
